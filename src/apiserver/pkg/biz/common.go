d2efc0f41cbf714bae05141da7f9fbc649a47414
wklken <wklken@gmail.com>
2026-01-29 19:39:56 +0800

    revert(common.go): should not be changed?

--- src/apiserver/pkg/biz/common.go
+++ src/apiserver/pkg/biz/common.go
@@ -852,9 +852,7 @@ func UpdateResource(
 		return fmt.Errorf("unsupported resource type: %v", resourceType)
 	}
 	newResourceModel := reflect.New(reflect.TypeOf(resourceModel).Elem()).Interface()
-	// ToResourceModel returns a pointer, need to dereference it
-	resourcePtr := resource.ToResourceModel(resourceType)
-	reflect.ValueOf(newResourceModel).Elem().Set(reflect.ValueOf(resourcePtr).Elem())
+	reflect.ValueOf(newResourceModel).Elem().Set(reflect.ValueOf(resource.ToResourceModel(resourceType)))
 	return buildCommonDbQuery(ctx, resourceType).Where("id = ?", id).Updates(newResourceModel).Error
 }
 
